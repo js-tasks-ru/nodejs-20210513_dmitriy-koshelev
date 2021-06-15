269e02fae2b1b986e74ab41d965191da4edd787a
gitkoshelew <gitkoshelew@gmail.com>
2021-06-16 02:35:28 +0300

    were added streams tasks solutions module 3

--- 03-streams/01-limit-size-stream/LimitSizeStream.js
+++ 03-streams/01-limit-size-stream/LimitSizeStream.js
@@ -3,11 +3,25 @@ const LimitExceededError = require('./LimitExceededError');
 
 class LimitSizeStream extends stream.Transform {
   constructor(options) {
-    super(options);
+    const decodeStrings =
+      options && options.hasOwnProperty('decodeStrings') ? options.decodeStrings : false;
+    super({...options, decodeStrings});
+    this._limit = options.limit;
+    this._chunksLength = 0;
   }
 
   _transform(chunk, encoding, callback) {
+    let error;
+    try {
+      this._chunksLength += Buffer.from(chunk, encoding).length;
 
+      if (this._limit !== undefined && this._limit < this._chunksLength) {
+        return callback(new LimitExceededError());
+      }
+    } catch (e) {
+      error = new LimitExceededError(e.message);
+    }
+    callback(error, chunk);
   }
 }
 
